770b45069c07103742750680f54320aa5f6bcdb7
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-07-27 23:53:31 +0530

    exercise.py

--- Day2/exercise.py
+++ Day2/exercise.py
@@ -1,13 +1,21 @@
-"""for i in range(0, 10):
-    print(i)"""
+#Number Guessing Game
 
-num = int(input("Enter a number: "))
+import random
+secret_number = random.randint(1, 100)
+attempts = 0
 
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
+while True:
+    guess = int(input("Guess the secret number between 1 and 100: "))
+    """if guess < 1 or guess > 100:
+        print("Please enter a number between 1 and 100.")
+        continue"""
+    attempts += 1
 
-"""count = 1
+    if guess < secret_number :
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You've guessed the secret number {secret_number} in {attempts} attempts.")
+        break
 
-while count <= 10:
-    print(f"{num} x {count} = {num * count}")
-    count += 1"""
