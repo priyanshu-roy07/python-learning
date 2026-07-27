be4f79010b831499d0231884d5782bf54c1c132f
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-07-27 22:31:41 +0530

    exercise.py

---
+++ Day2/exercise.py
@@ -0,0 +1,13 @@
+"""for i in range(0, 10):
+    print(i)"""
+
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+"""count = 1
+
+while count <= 10:
+    print(f"{num} x {count} = {num * count}")
+    count += 1"""
