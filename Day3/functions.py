e643675da0397229ba9382e9c3359e991951a525
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-07-30 23:05:10 +0530

    functions.py

---
+++ Day3/functions.py
@@ -0,0 +1,22 @@
+"""def my_function():
+    print("Hello from my_function!")
+my_function()  # Calling the function to execute it"""
+
+#function for square of a number
+"""num = int(input("Enter a number to square: "))
+def square(num):
+    return num ** 2
+result = square(num)
+print(f"The square of the number {num} is {result}.")"""
+
+#funtion to check if a number is even or odd
+
+num = int(input("Enter number here to check if it's even or odd: "))
+def is_even():
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+result = is_even()
+print(f"The number {num} is {result}")
