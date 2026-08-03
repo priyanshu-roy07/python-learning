1b5024e684ee879caa5dcee4b084250600786f2c
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-03 23:21:44 +0530

    dictionaries.py

---
+++ Day4/dictionaries.py
@@ -0,0 +1,17 @@
+"""student = {
+    "name": "Priyanshu",
+    "age": 23,
+    "branch": "CSE",
+    "marks": 92
+}
+student["marks"] = 98
+student["college"] = "SU"
+
+print(student["college"])"""
+
+student = {
+    "name": "Priyanshu",
+    "age": 23
+}
+
+print(len(student))
