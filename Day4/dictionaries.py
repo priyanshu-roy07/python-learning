7b0f82b7eee6e27d4bf88567929976b6062ddac6
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-04 23:22:37 +0530

    dictionaries.py

--- Day4/dictionaries.py
+++ Day4/dictionaries.py
@@ -9,9 +9,34 @@ student["college"] = "SU"
 
 print(student["college"])"""
 
-student = {
+"""student = {
     "name": "Priyanshu",
     "age": 23
 }
+print(len(student))"""
 
-print(len(student))
+# MINI CHALLENGE
+"""employee = {
+    "id": 101,
+    "name": "Alice",
+    "salary": 50000,
+    "department": "IT"
+}
+employee["salary"] = 55000
+employee['city'] = "Banglore"
+print(f"Name of the employee is {employee["name"]} and salary is {employee['salary']}")
+print(employee.keys())
+print(employee.values())"""
+
+# MINI PROJECT - Student Database
+students = {}
+
+while True:
+    name = input("Enter your name: ")
+    marks = int(input("Enter your marks: "))
+    students[name] = marks
+    choice = input("Add another? y/n ")
+    if choice == "n":
+        break
+for key, value in students.items():
+    print(key,":", value)
