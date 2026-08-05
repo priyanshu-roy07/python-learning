a459dbdaf6590835a62055800d24d6575863b402
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-05 23:41:02 +0530

    tuples.py

---
+++ Day5/tuples.py
@@ -0,0 +1,9 @@
+"""student = ("Rahul", 21, 87)
+print(student[0])
+print(student[2])"""
+
+#Tuples only have two methods
+"""student = ("Priyanshu",22,"CSE")
+print(student.count(22))
+print(student.index(22))
+print(student)"""
