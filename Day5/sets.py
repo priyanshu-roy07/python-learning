5c2c7bfa126b4f1532ad5e5c8a7351a7672707be
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-05 23:54:31 +0530

    sets.py

---
+++ Day5/sets.py
@@ -0,0 +1,32 @@
+#Duplicates are automatically removed from sets
+"""numbers = {1,2,3,2,1,5}
+print(numbers)
+"""
+#Add/Remove items
+"""fruits = {"apple","banana"}
+fruits.add("mango")
+fruits.remove("banana")
+print(fruits)
+
+#Membership
+if "apple" in fruits:
+    print("Found")"""
+
+#UNION, INTERSECTION, DIFFERENCE
+"""A = {1,2,3}
+B = {3,4,5}
+
+print(A | B) #union
+print(A & B) #intersection
+print(A - B) #difference"""
+
+#practice
+"""cities = {"Delhi","Mumbai","Delhi","Pune","Mumbai"}
+print(cities)"""
+
+#practice2
+A = {10,20,30,40}
+B = {30,40,50}
+print(A | B)
+print(A & B)
+print(A - B)
