fe116de6e3932da22de4556352d676679e51b565
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-10 23:20:09 +0530

    oop_basics.py

--- Day7/oop_basics.py
+++ Day7/oop_basics.py
@@ -11,15 +11,79 @@ print(student1.name, student1.marks)
 print(student2.name, student2.marks)
 """
 
+
+#NOTE: Function → independent
+#      Method   → function belonging to a class/object 
+
 #PRACTICE
-class Car:
+"""class Car:
     def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
         self.year = year
+    def display_info(self):             #Method used here (A method is simply a function that belongs to a class/object.)
+        print(self.brand, self.model, self.year)
+
 
 car1 = Car("Toyota", "Corolla", 2022)
 car2 = Car("Honda", "City", 2024)
 
-print(car1.brand, car1.model, car1.year)
-print(car2.brand, car2.model, car2.year)
+car1.display_info()         #printing directly using method created earlier
+car2.display_info()
+
+#print(car1.brand, car1.model, car1.year)
+#print(car2.brand, car2.model, car2.year)"""
+
+#EXERCISE
+
+"""class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+    def display_balance(self):
+        print(f"{self.owner}'s balance: {self.balance}")
+    def deposit(self, amount):
+        self.balance += amount
+        #print(f"{self.owner}'s balance: {self.balance}")
+    def withdraw(self, amount):
+        self.balance -= amount
+        #print(f"{self.owner}'s balance: {self.balance}")
+
+account_details = BankAccount("Priyanshu", 50000)
+
+account_details.display_balance()
+account_details.deposit(10000)
+account_details.display_balance()
+account_details.withdraw(5000)
+account_details.display_balance()
+"""
+
+#ENCAPSULATION
+#Encapsulation means controlling how the internal data of an object can be accessed or changed.
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self._balance = balance
+    def display_balance(self):
+        print(f"{self.owner}'s balance: {self._balance}")
+    def deposit(self, amount):
+        self._balance += amount
+    def withdraw(self, amount):
+        if amount <= self._balance:
+            self._balance -= amount
+        else:
+            print("Insufficient balance!")
+
+account_details = BankAccount("Priyanshu", 50000)
+
+account_details.display_balance()
+
+account_details.deposit(10000)
+account_details.display_balance()
+
+account_details.withdraw(5000)
+account_details.display_balance()
+
+account_details.withdraw(60000)
+account_details.display_balance()
