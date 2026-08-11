dc0ab6901a9a2f68e71345473730e1df9d7d5257
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-11 20:29:53 +0530

    oop_basics.py

--- Day7/oop_basics.py
+++ Day7/oop_basics.py
@@ -61,7 +61,7 @@ account_details.display_balance()
 #ENCAPSULATION
 #Encapsulation means controlling how the internal data of an object can be accessed or changed.
 
-class BankAccount:
+"""class BankAccount:
     def __init__(self, owner, balance):
         self.owner = owner
         self._balance = balance
@@ -86,4 +86,54 @@ account_details.withdraw(5000)
 account_details.display_balance()
 
 account_details.withdraw(60000)
-account_details.display_balance()
+account_details.display_balance()"""
+
+
+#INHERITANCE
+#Inheritance is a mechanism in object-oriented programming that allows a class (called the child or subclass) to inherit properties and behaviors (attributes and methods) from another class (called the parent or superclass). This promotes code reusability and establishes a hierarchical relationship between classes.
+
+"""class Student:
+    def __init__(self, name, roll_number):
+        self.name = name
+        self.roll_number = roll_number
+    def display_info(self):
+        print(f"Student: {self.name}, roll no. {self.roll_number}")
+
+class Engineering_student(Student):
+    def __init__(self,name, roll_number, programming_language):
+        super().__init__(name, roll_number)
+        self.programming_language = programming_language
+    def display_specialization(self):
+        print(self.programming_language)
+
+student1 = Engineering_student("Priyanshu", 7, "Python")
+
+student1.display_info()
+student1.display_specialization()"""
+
+
+#POLYMORPHISM
+#Polymorphism is a concept in object-oriented programming that allows objects of different classes to be treated as objects of a common superclass. It enables a single interface to represent different types of objects, allowing for flexibility and code reusability.
+
+class Student:
+    def display_info(self):
+            print("General student course")
+
+class EngineeringStudent(Student):
+    def display_info(self):
+        print("Computer Science Engineering")
+
+class MedicalStudent(Student):
+        def display_info(self):
+            print("Medicine")
+
+student1 = EngineeringStudent()
+student2 = MedicalStudent()
+
+students = [student1, student2]
+
+for student in students:
+     student.display_info()
+
+#student1.display_info()
+#student2.display_info()
