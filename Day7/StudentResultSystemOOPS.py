af1e9086b0a16613986b4c776512b1347904d5ad
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-08-16 18:42:02 +0530

    StudentResultSystemOOPS.py

--- Day7/StudentResultSystemOOPS.py
+++ Day7/StudentResultSystemOOPS.py
@@ -27,20 +27,50 @@ student3 = Student("Bumba", [78, 67, 86])
 
 #students = [student1, student2, student3]
 
-student2.update_marks([56,44,65])
+#student2.update_marks([56,44,65])
 
 class StudentManager:
     def __init__(self):
         self.students = []
     def add_student(self, student):
         self.students.append(student)
-
+    def view_students(self):
+        for student in self.students:
+            student.display_info()
+            print(f"Average of marks: {student.calculate_average()} -> {student.calculate_grade()}")
+    def search_students(self, search_name):
+        found = False
+        for student in self.students:
+            if search_name == student.name:
+                student.display_info()
+                print(f"Average of marks: {student.calculate_average()} -> Grade {student.calculate_grade()}")
+                print("Student found!")
+                found = True
+        if found is False:
+            print("Student not found!")
+    def update_student_marks(self, student_name, new_marks):
+        found = False
+        for student in self.students:
+            if student_name == student.name:
+                student.update_marks(new_marks)
+                found = True
+        if found is False:
+            print("Student not found!")
+    def remove_student(self, student_name):
+        found = False
+        for student in self.students:
+            if student_name == student.name:
+                self.students.remove(student)
+                found = True
+        if found is False:
+            print("Student not found!")
 manager = StudentManager()
 manager.add_student(student1)
 manager.add_student(student2)
 manager.add_student(student3)
-print(manager.students)
-
-for student in manager.students:
-    student.display_info()
-    print(f"Average of marks: {student.calculate_average()} -> {student.calculate_grade()}")
+"""manager.search_students("Priyanshu")
+#manager.search_students("Shivam")
+manager.update_student_marks("Aman", [87, 78, 77])
+manager.search_students("Aman")"""
+manager.remove_student("Bumba")
+manager.view_students()
