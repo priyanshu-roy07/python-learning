1d38cc5a8829c57e6a828fde61309b2afd52706a
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-09-02 23:33:55 +0530

    day8_file_handling.py

--- Day8/day8_file_handling.py
+++ Day8/day8_file_handling.py
@@ -1,11 +1,22 @@
-file = open("notes.txt", "w")
+"""file = open("notes.txt", "w")
 file.write("Python!")
 file.close()
 
 file = open("notes.txt", "a")
 file.write("\nFile handling.")
 file.close()
-
-file = open("notes.txt", "r")
+"""
+"""file = open("notes.txt", "r")
 print(file.read())
-file.close()
+file.close()"""
+
+"""with open("notes.txt", "r") as file:
+    #print(file.readline())
+    print(file.readlines())
+    #print(file.read())
+"""
+with open("notes.txt", "r") as file:
+    lines = file.readlines()
+print(lines[0])
+#for line in lines:
+ #   print(line)
--- Day8/tempCodeRunnerFile.py
+++ Day8/tempCodeRunnerFile.py
@@ -1,3 +1,3 @@
-file = open("notes.txt", "w")
-file.write("Hello Python!")
-file.close
+file = open("notes.txt", "r")
+print(file.read())
+file.close()
