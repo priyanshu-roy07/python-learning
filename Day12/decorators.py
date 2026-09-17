abe6ab5f0727c11e53eb524e40529a98a2c2f649
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2026-09-17 23:22:59 +0530

    decorators.py

---
+++ Day12/decorators.py
@@ -0,0 +1,14 @@
+"""def greet():
+    print("Hello Priyanshu")
+
+say_hello = greet
+say_hello()"""
+
+
+def say_name():
+    print("Priyanshu")
+
+def run_function(function):
+    function()
+
+run_function(say_name)
